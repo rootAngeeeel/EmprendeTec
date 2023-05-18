a9664596a1a6ec18ae28bdfaa6aa1fedd38e0371
Angel <soldadoazteca@live.com.mx>
2023-05-17 20:02:06 -0600

    Commit: Se agrego la activity AreasInteres y se completo la activity RedesSociales

---
+++ app/src/main/java/com/example/emprendetec/register_areasInteres.java
@@ -0,0 +1,14 @@
+package com.example.emprendetec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class register_areasInteres extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_areas_interes);
+    }
+}
